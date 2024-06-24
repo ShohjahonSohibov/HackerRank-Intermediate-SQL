f9d584f1fd8f38dc0ef6d60fec4e955b0274ec57
shohjahonsohibov0 <shohjahonsohibov0@gmail.com>
2024-06-24 15:32:44 +0500

    challenge 2

---
+++ Binary Tree Nodes.sql
@@ -0,0 +1,18 @@
+// Binary Tree Nodes
+
+// Link to challenge: https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
+
+SELECT  b.n,  
+    CASE 
+        WHEN b.p IS NOT NULL THEN
+            CASE     
+                    WHEN (SELECT count(*) from bst where p = b.n) = 0
+                    THEN 'Leaf'
+                    ELSE 'Inner'
+            END
+        ELSE 
+            'Root'
+    END
+FROM bst b
+ORDER BY b.n
