622b9369c5e63860d3b119ae0df187ec32d2c901
Shohjahon | Umar Forsiy <155833639+ShohjahonSohibov@users.noreply.github.com>
2024-06-30 00:02:44 +0500

    write hint

--- Contest Leaderboard.sql
+++ Contest Leaderboard.sql
@@ -11,3 +11,7 @@ inner join ( select max(s.score) as sc, s.hacker_id from submissions s group by 
         on h.hacker_id = max_score.hacker_id
 group by hacker_id, h.name
 order by total_score desc, hacker_id asc;
+
+-- Hint: 1. select start_date which does not exist in end_date, 
+      -- 2. select end_date which does not exist in start_date, 
+      -- 3. order by score desc, and hacker_id asc for ordering hackers by hacker_id asc if their score are the same 
